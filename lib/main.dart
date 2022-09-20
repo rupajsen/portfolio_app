22ad786efe4e35b4f9073e1024aed6f77ad82e44
Rupaj Sen <rupajps4@gmail.com>
2022-09-21 00:09:10 +0530

    updt.2

--- lib/main.dart
+++ lib/main.dart
@@ -35,42 +35,53 @@ class Home extends StatelessWidget {
                 Row(
                   children: <Widget>[
                     Icon(Icons.school,size: 40,),
-                    SizedBox(width: 20,),
+                    SizedBox(width: 25,),
+                    
                     Text("School Name",style: TextStyle(fontSize: 20),)
-                  ],
+                  ]
                 ),
+                SizedBox(height: 10,),
                 Row(
                   children: <Widget>[
                     Icon(Icons.computer_rounded,size: 40,),
-                    SizedBox(width: 20,),
+                    SizedBox(width: 25,),
                     Text("School Name",style: TextStyle(fontSize: 20),)
                   ],
                 ),
+                SizedBox(height: 10,),
                 Row(
                   children: <Widget>[
                     Icon(Icons.location_pin,size: 40,),
-                    SizedBox(width: 20,),
+                    SizedBox(width: 25,),
                     Text("School Name",style: TextStyle(fontSize: 20),)
                   ],
                 ),
+                SizedBox(height: 10,),
                 Row(
                   children: <Widget>[
                     Icon(Icons.email,size: 40,),
-                    SizedBox(width: 20,),
+                    SizedBox(width: 25,),
                     Text("School Name",style: TextStyle(fontSize: 20),)
                   ],
                 ),
+                SizedBox(height: 10,),
                 Row(
                   children: <Widget>[
                     Icon(Icons.phone,size: 40,),
-                    SizedBox(width: 20,),
+                    SizedBox(width: 25,),
                     Text("School Name",style: TextStyle(fontSize: 20),)
                   ],
                 ),
+                SizedBox(height: 10,),
               ],
              ),
            ),
-           Text("About Me"),
+           SizedBox(height: 50,),
+           Padding(
+             padding: const EdgeInsets.all(10.0),
+             child: Text("Hi I'm Rupaj Sen from India, and I solve problems in creative ways. I am currently a second-year B.Tech student pursuing Computer Science and Engineering at SRM University. Aspiring to become a good professional.",style: TextStyle(fontSize: 22),),
+           ),
+           SizedBox(height: 10,),
            Text("Created by Rupaj"),
           ],
         ),
