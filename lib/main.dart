4c20bdea1762a949bcdc95dab8e3f9274fbe00ef
Rupaj Sen <rupajps4@gmail.com>
2022-09-20 23:22:49 +0530

    updt.

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,61 @@ class Home extends StatelessWidget {
            Row(
             children: <Widget>[
               CircleAvatar(radius: 60,),
+              SizedBox(width: 50,),
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Name"),
-                  Text("Designation")
+                  Text("Rupaj Sen",style: TextStyle(fontSize: 30),),
+                  Text("App Developer",style: TextStyle(fontSize: 18),)
                 ],
               )
             ],
-           )
+           ),
+           SizedBox(height: 50,),
+           Padding(
+             padding: const EdgeInsets.only(left:30),
+             child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.school,size: 40,),
+                    SizedBox(width: 20,),
+                    Text("School Name",style: TextStyle(fontSize: 20),)
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.computer_rounded,size: 40,),
+                    SizedBox(width: 20,),
+                    Text("School Name",style: TextStyle(fontSize: 20),)
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.location_pin,size: 40,),
+                    SizedBox(width: 20,),
+                    Text("School Name",style: TextStyle(fontSize: 20),)
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.email,size: 40,),
+                    SizedBox(width: 20,),
+                    Text("School Name",style: TextStyle(fontSize: 20),)
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.phone,size: 40,),
+                    SizedBox(width: 20,),
+                    Text("School Name",style: TextStyle(fontSize: 20),)
+                  ],
+                ),
+              ],
+             ),
+           ),
+           Text("About Me"),
+           Text("Created by Rupaj"),
           ],
         ),
       ),
